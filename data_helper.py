86e4bc11e52645cb846ab5b12d6fca7dcf2ad525
matthew quinn <matthewq1428@gmail.com>
2023-05-10 09:36:51 -0400

    Work through missing values technique

--- data_helper.py
+++ data_helper.py
@@ -1,11 +1,11 @@
+import random
 import pandas as pd
-
 
 from faker import Faker
 from bson import ObjectId
 
 
-def generate_fake_data(n_rows=25):
+def generate_users(n_rows=25):
     fake = Faker()
     data = []
     for i in range(n_rows):
@@ -21,3 +21,31 @@ def generate_fake_data(n_rows=25):
         data, columns=["ID", "name", "AgE", "dateOfBirth", "current_COUNTRY"]
     )
     return df
+
+
+def generate_states(num_rows=50):
+    states = ["Ohio", "Michigan", "Alabama", "Texas"]
+    state_abbrevs = ["OH", "MI", "AL", "TX"]
+    state_data = []
+
+    for i in range(num_rows):
+        state = random.choice(states)
+        state_abbrev = state_abbrevs[states.index(state)]
+        state_data.append((ObjectId(), state_abbrev))
+        state_data.append((ObjectId(), state))
+
+    df = pd.DataFrame(state_data, columns=["id", "state"])
+    return df.sample(frac=1).reset_index(drop=True)
+
+
+def generate_gender(n_row=100):
+    genders = ["Man", "Woman", "DND"]
+    data = {
+        "gender": [
+            random.choice(genders).lower()
+            if random.randint(0, 1) == 0
+            else random.choice(genders).upper()
+            for _ in range(n_row)
+        ]
+    }
+    return pd.DataFrame(data)
